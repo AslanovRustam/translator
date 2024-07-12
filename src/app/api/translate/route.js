c87764fff562f114617d33e0e0d5b67e72863d2e
Aslanov Rustam <aslanovrg@gmail.com>
2024-07-12 15:17:36 +0300

    add voice recognition, add axios, add antd

--- src/app/api/translate/route.js
+++ src/app/api/translate/route.js
@@ -1,9 +1,10 @@
 import { NextResponse } from "next/server";
+import { translate } from "@vitalets/google-translate-api";
 
 export async function POST(request) {
   const { text, language } = await request.json();
-
-  const data = `${text} на ${language} языке`;
-
-  return NextResponse.json({ text: data });
+  const translatedText = await translate(text, {
+    to: language,
+  });
+  return NextResponse.json({ text: translatedText.text });
 }
--- src/components/Translator.jsx
+++ src/components/Translator.jsx
@@ -1,12 +1,25 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import s from "./translator.module.css";
+import { Button, Flex, Typography, Input, Select } from "antd";
+import { languages } from "@/data/language";
+
+const { Title, Paragraph, Text } = Typography;
+const { TextArea } = Input;
 
 function Translator() {
   const [text, setText] = useState("");
-  const [translation, setTranslation] = useState("");
+  const [selectedLanguage, setSelectedLanguage] = useState("en");
   const [isActive, setIsActive] = useState(false);
+  const [isTranslating, setTranslating] = useState(false);
+  const [translation, setTranslation] = useState("");
+  const [error, setError] = useState(null);
+  const [position, setPosition] = useState("end");
+
+  useEffect(() => {
+    let utterance = new SpeechSynthesisUtterance(translation);
+    window.speechSynthesis.speak(utterance);
+  }, [translation]);
 
   const handleOnRecord = () => {
     const SpeechRecognition =
@@ -15,29 +28,98 @@ function Translator() {
     const recognition = new SpeechRecognition();
 
     recognition.start();
+    setIsActive(true);
 
     recognition.onresult = async (event) => {
       const transcript = event.results[0][0].transcript;
-      setText((prev) => `${prev} ${transcript}`);
-
-      const { data } = await axios.post("/api/translate", {
-        text: transcript,
-        language: "en-US",
-      });
-      setTranslation(data.text);
+      const textToTranslate = `${text} ${transcript}`;
+      setText(textToTranslate);
+      try {
+        const { data } = await axios.post("/api/translate", {
+          text: textToTranslate,
+          language: selectedLanguage,
+        });
+        setTranslation(data.text);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsActive(false);
+      }
     };
-    setIsActive(!isActive);
+  };
+
+  const onTextAreaChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSelectChange = (value) => {
+    setSelectedLanguage(value);
+  };
+
+  const onTranslate = () => {
+    setTranslating(true);
+    (async function getTranslate() {
+      try {
+        const { data } = await axios.post("/api/translate", {
+          text: text,
+          language: selectedLanguage,
+        });
+        setTranslation(data.text);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setTranslating(false);
+      }
+    })();
   };
 
   return (
-    <div className={s.container}>
-      Translator
-      <textarea type="area" className={s.area} rows="5" value={text} />
-      <p>{translation}</p>
-      <button type="button" onClick={handleOnRecord} className={s.btn}>
-        {isActive ? "Stop" : "Record"}
-      </button>
-    </div>
+    <Flex vertical gap={16} className="container">
+      <Title style={{ textTransform: "uppercase" }}> Translator</Title>
+      <Select
+        showSearch
+        onChange={onSelectChange}
+        defaultValue={selectedLanguage}
+      >
+        {languages.map((item) =>
+          Object.entries(item).map(([key, value]) => (
+            <Select.Option value={key}>{value}</Select.Option>
+          ))
+        )}
+      </Select>
+      <TextArea
+        rows={4}
+        placeholder="maxLength is 6"
+        autoSize={{ minRows: 4 }}
+        value={text}
+        onChange={onTextAreaChange}
+      />
+      {error && <Paragraph>Something went wrong, {error}</Paragraph>}
+      <Flex gap="middle" justify="space-evenly">
+        <Button
+          type="primary"
+          iconPosition={position}
+          loading={isTranslating ? true : false}
+          onClick={onTranslate}
+          className="button"
+        >
+          Translate
+        </Button>
+        <Button
+          type="primary"
+          danger={isActive ? true : false}
+          onClick={handleOnRecord}
+          className="button"
+        >
+          {isActive ? "Stop" : "Record"}
+        </Button>
+      </Flex>
+      {translation && (
+        <Text keyboard className="translations">
+          {translation}
+        </Text>
+      )}
+    </Flex>
   );
 }
 
